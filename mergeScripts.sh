a9d9865e7c4c953cd240707975ad5cd3dbd473c5
Jaz Singh <jaz@color.com>
2022-11-13 18:32:21 -0800

    Fix mergeScripts.sh

--- mergeScripts.sh
+++ mergeScripts.sh
@@ -4,7 +4,11 @@ set -eux -o pipefail
 
 currentDirectory=$(dirname "${BASH_SOURCE:-$0}")
 
-find "${currentDirectory}/ui/scripts" -name '*.js' -exec cat {} \; > "${currentDirectory}/ui/bin/roll20.js"
+targetDirectory="${currentDirectory}/ui/bin/"
+
+mkdir -p "${targetDirectory}"
+
+find "${currentDirectory}/ui/scripts" -name '*.js' -exec cat {} \; > "${targetDirectory}/roll20.js"
 
 echo "Done"
 
