63eccd55fdcbce8680e223cc35e077d611b0e7a4
Jaz <jazeee@gmail.com>
2022-11-26 18:54:27 -0800

    Run github action only on user branches (#14)
    `main` branch is protected, so commits to it fail.

--- ui/bin/roll20.js
+++ ui/bin/roll20.js
@@ -1,3 +1,10 @@
+(function() {
+  /**
+   * This is a test function that has no effect.
+   * Just testing the mergeScripts.sh
+   * This is also an example of an IIFE, which allows each script to be self-contained to reduce risks of collisions between scripts.
+   */
+})();
 // eslint-disable-next-line no-unused-vars
 const prevEPRoll = function() {
   let total = 0;
@@ -217,10 +224,3 @@ on('change:attribute', function(
     }
   }
 });
-(function() {
-  /**
-   * This is a test function that has no effect.
-   * Just testing the mergeScripts.sh
-   * This is also an example of an IIFE, which allows each script to be self-contained to reduce risks of collisions between scripts.
-   */
-})();
