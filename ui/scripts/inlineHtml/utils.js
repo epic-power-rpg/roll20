4123c1b287267af8a737cfbc56490e5e0e72fc04
Jaz Singh <jazeee@gmail.com>
2024-03-02 18:11:40 -0800

    Add comment

--- ui/scripts/inlineHtml/utils.js
+++ ui/scripts/inlineHtml/utils.js
@@ -1,5 +1,10 @@
 const uniqueIds = new Set();
 
+/**
+ * generateRowID() doesn't always generate a unique value.
+ * See:
+ * https://app.roll20.net/forum/post/8879969/generaterowid-does-not-always-generate-a-star-unique-star-rowid
+ */
 // eslint-disable-next-line no-unused-vars
 function generateTrulyUniqueRowId() {
   let newId = generateRowID();
